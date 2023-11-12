c8cc67eb6b0abb25145a1ad133f75faa6531b8e9
Zarina Saidova Poyet <zarina.poyet@gmail.com>
2023-11-12 13:57:21 +0100

    style: tennisClub basic style

--- client/src/components/tennisClub/TennisClub.js
+++ client/src/components/tennisClub/TennisClub.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
 import { useFilters } from '../../context/FiltersContext';
 import { getClubs } from '../../services/clubs';
+import { MdArrowBack } from 'react-icons/md';
 import './tennisClub.css'
 import logo from '../../assets/Logo_orange.svg'
 
@@ -31,21 +32,27 @@ const TennisClub = () => {
         <div className='tennisClub-container'>
             <img src={logo} alt="Logo" className='logo' />
             <h3 className="app-name">RacketMate</h3>
-            <Link to="/dashboard/skill-level">⬅️</Link>
-            <p>step 3 of 3</p>
+            <div className="navigation-container">
+                <Link to="/dashboard/skill-level" className="back-arrow"><MdArrowBack /></Link>
+                <p className='step-info'>step 3 of 3</p>
+            </div>
             <h2>Tennis Club</h2>
-            <select
-                value={filters.club || ''}
-                onChange={(e) => setFilters(prev => ({ ...prev, club: e.target.value }))}
-            >
-                <option value='' disabled>Select a club</option>
-                {clubs.map((clubItem) => (
-                    <option key={clubItem._id} value={clubItem.name}>
-                        {clubItem.name}
-                    </option>
-                ))}
-            </select>
-            <button onClick={handleNext}>Next</button>
+            <div className="select-action-container">
+                <div className="select-container">
+                    <select
+                        value={filters.club || ''}
+                        onChange={(e) => setFilters(prev => ({ ...prev, club: e.target.value }))}
+                    >
+                        <option value='' disabled>Select a club</option>
+                        {clubs.map((clubItem) => (
+                            <option key={clubItem._id} value={clubItem.name}>
+                                {clubItem.name}
+                            </option>
+                        ))}
+                    </select>
+                </div>
+                <button onClick={handleNext}>Next</button>
+            </div>
         </div>
     );
 };
