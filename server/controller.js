55df080ea8edc75da45df4c848210dae1ef6f471
Zarina Saidova Poyet <zarina.poyet@gmail.com>
2023-11-13 23:30:45 +0100

    feat: logout done

--- server/controller.js
+++ server/controller.js
@@ -132,7 +132,14 @@ const registerUser = asyncHandler(async (req, res) => {
 // @access   Public 
 
 const logoutUser = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: 'Logout User' })
+    res.cookie('jwt', '', {
+        httpOnly: true,
+        expires: new Date(0)
+    })
+
+
+
+    res.status(200).json({ message: 'User logged out' })
 });
 
 // @desc     Get user profile
