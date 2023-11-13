d6b6c974711971b520ba3a48c09c294f9157e0a2
Zarina Saidova Poyet <zarina.poyet@gmail.com>
2023-11-14 00:08:10 +0100

    style: profile style

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -1,15 +1,30 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { MdArrowBack } from 'react-icons/md';
+import './profile.css'
+import logo from '../../assets/Logo_orange.svg'
 
 const Profile = () => {
     return (
-        <div>
-            <Link to="/dashboard">⬅️</Link>
-            <h2>Bat Poyet</h2>
-            <p>Gender: Male</p>
-            <p>Skill Level: Professional</p>
-            <p>Club: Real Club de Tenis Barcelona</p>
-            <Link to="/profile/edit"><button>Edit</button></Link>
+        <div className='profile-container'>
+            <img src={logo} alt="Logo" className='logo' />
+            <h3 className="app-name">RacketMate</h3>
+            <Link to="/dashboard" className="back-arrow-detail"><MdArrowBack /></Link>
+            <div className="card">
+                <div className="initial-container">
+                    <div className='initial'>B</div>
+                    <Link to="/profile/edit"><button className='msg-button'>Edit</button></Link>
+                </div>
+                <div className="details">
+                    <div className='name-surname-detail'>Bat Poyet</div>
+                    <div className="gender-skill-club">
+                        <p><span>Gender: </span> <span>Male</span></p>
+                        <p><span>Level: </span> <span>Professional</span></p>
+                        <p><span>Club: </span> <span>Real Club de Tenis Barcelona</span></p>
+                    </div>
+                </div>
+            </div>
+
         </div>
     )
 }
